7a169b94e537bd634703609e0fef0793ed3ffad8
Adam Lukens <adam@testdouble.com>
2016-12-02 00:03:36 -0500

    Added a wrapping script that guides a bit more of how to use this

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$RIP_TITLE" ]; then
+  echo "Missing required environment variable RIP_TITLE"
+  cat <<EOF
+    ENVIRONMENT VARIABLES:
+    * RIP_TITLE      Name of the movie/show. Required.
+    * RIP_TITLE_ID   The title id of the disk to rip. Optional.
+                     Use -1 for the longest title.
+		     When missing, it rips all titles.
+    * RIP_DEVICE     Which device to read from when ripping.
+                     Useful for multiple disk drives.
+
+    Creates a volume '/videos' where it stores videos. Either use Docker to
+    bind that to a local directory (e.g. '-v ~/Videos:/videos') or transfer
+    files out after it's completed.
+EOF
+  exit 1
+fi
+
+./rip_disk.rb && ./transcode_dir.rb "$RIP_TITLE"
