c322c769d4e1c6ca481d81bc50805034108b7a2c
Adam Lukens <adam@testdouble.com>
2020-06-20 20:03:25 -0400

    Fix script to update MakeMKV beta key

--- update_makemkv_beta_key.sh
+++ update_makemkv_beta_key.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-curl -s 'http://makemkv.com/forum2/viewtopic.php?f=5&t=1053' | grep -oP '<div class="codecontent">\K[^<]+'
+CODE=$(curl -Ls 'http://makemkv.com/forum2/viewtopic.php?f=5&t=1053' | grep -oP '<code>[^<]+' | cut -d '>' -f 2-)
+
+echo "Update ~/.MakeMKV/settings.conf with "
+echo "app_Key = \"${CODE}\""
