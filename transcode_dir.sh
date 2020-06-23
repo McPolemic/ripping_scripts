437ccab150164cd1d4fb2b5998cfdacb6183f4b3
Adam Lukens <adam@testdouble.com>
2020-06-23 01:34:06 -0400

    Fix globbing in transcode_dir.sh

--- transcode_dir.sh
+++ transcode_dir.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+shopt -s nullglob
+
 cd "$1"
-for mkv in *.mkv; do
-  OUTPUT="$(basename ${mkv} .mkv).mp4"
+
+for mkv in *.mkv **/*.mkv; do
+  echo "Checking ${mkv}..."
+  DIRNAME="$(dirname ${mkv})"
+  OUTPUT="${DIRNAME}/$(basename ${mkv} .mkv).mp4"
+  if [ -f ${OUTPUT} ]; then
+    continue
+  fi
+  echo "Converting ${mkv}..."
+
   HandBrakeCLI --preset "High Profile" -i ${mkv} -o ${OUTPUT}
 done
