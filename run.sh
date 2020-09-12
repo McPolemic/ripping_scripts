c3a8d13fbe35296c08733940bbc3ea8de2b9fb40
Raelyn Bangel <raelyn@testdouble.com>
2020-09-12 12:45:25 -0500

    Use environment variable for preset

--- run.sh
+++ run.sh
@@ -4,12 +4,14 @@ if [ -z "$RIP_TITLE" ]; then
   echo "Missing required environment variable RIP_TITLE"
   cat <<EOF
     ENVIRONMENT VARIABLES:
-    * RIP_TITLE      Name of the movie/show. Required.
-    * RIP_TITLE_ID   The title id of the disk to rip. Optional.
-                     Use -1 for the longest title.
-		     When missing, it rips all titles.
-    * RIP_DEVICE     Which device to read from when ripping.
-                     Useful for multiple disk drives.
+    * RIP_TITLE           Name of the movie/show. Required.
+    * RIP_TITLE_ID        The title id of the disk to rip. Optional.
+                          Use -1 for the longest title.
+		                      default: all titles
+    * RIP_DEVICE          Which device to read from when ripping.
+                          Useful for multiple disk drives.
+    * TRANSCODE_PRESET    Which device to read from when ripping.
+                          Useful for multiple disk drives.
 
     Creates a volume '/videos' where it stores videos. Either use Docker to
     bind that to a local directory (e.g. '-v ~/Videos:/videos') or transfer
--- transcode_dir.rb
+++ transcode_dir.rb
@@ -2,13 +2,14 @@
 require 'fileutils'
 
 class MkvToMp4
-  def initialize(input, output)
+  def initialize(input, output, preset: 'High Profile')
     @input = input
     @output = output
+    @preset = preset
   end
 
   def perform()
-    `HandBrakeCLI --preset "High Profile" -i "#{@input}" -o "#{@output}"`
+    `HandBrakeCLI --preset #{@preset} -i "#{@input}" -o "#{@output}"`
     {input_file: @input, output_file: @output}
   end
 end
@@ -25,11 +26,13 @@ CONVERSION_DIR = '/videos/'
 
 title = ARGV.first.gsub(/[ ':\/\\]+/, "_")
 output_dir = File.join(CONVERSION_DIR, title)
+preset = ENV['TRANSCODE_PRESET'] || 'High Profile'
+
 FileUtils.mkdir_p(output_dir)
 
 input_files = Dir.glob(File.join(SOURCE_DIR, title, "**"))
 input_files.each do |input|
   output = input.gsub(SOURCE_DIR, CONVERSION_DIR).gsub('.mkv', '.mp4')
-  MkvToMp4.new(input, output).perform
+  MkvToMp4.new(input, output, preset: ).perform
 end
 
